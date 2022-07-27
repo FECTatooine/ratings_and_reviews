55c6a253b9d71688aabc5b1bb36993bb46fc8047
Jean Kim <jeankim119@gmail.com>
2022-07-27 15:19:33 -0500

    data from csv can be put into postgres

--- schema.sql
+++ schema.sql
@@ -1,27 +1,40 @@
-DROP TABLE IF EXISTS allReviews;
+DROP TABLE IF EXISTS allReviews CASCADE;
 CREATE TABLE allReviews (
     review_id serial PRIMARY KEY,
     product_id INTEGER NOT NULL,
     rating INTEGER NOT NULL,
     summary VARCHAR(60) NULL,
     body VARCHAR(1000) NOT NULL,
+    response VARCHAR(1000) NULL,
     recommend BOOLEAN NOT NULL,
     username VARCHAR(30) NOT NULL,
     email VARCHAR(30) NOT NULL,
-    photos TEXT,
     helpful INTEGER NOT NULL DEFAULT 0,
-    report INTEGER NOT NULL DEFAULT 0,
+    report BOOLEAN DEFAULT false,
     dateCreated TIMESTAMP
+);
+
+DROP TABLE IF EXISTS photos;
+CREATE TABLE photos (
+    photo_id serial PRIMARY KEY,
+    photo_url TEXT,
+    review_id INTEGER NOT NULL,
 );
 
 DROP TABLE IF EXISTS characteristics;
 CREATE TABLE characteristics (
+    char_id serial PRIMARY KEY,
+    product_id INTEGER NOT NULL,
+    characteristic_name TEXT
+);
+
+DROP TABLE IF EXISTS characteristic_ratings;
+CREATE TABLE characteristics (
     char_rating_id serial PRIMARY KEY,
     product_id INTEGER NOT NULL,
-    characteristic_id INTEGER NOT NULL,
+    char_id INTEGER NOT NULL,
     characteristic_rating INTEGER NOT NULL,
-    review_id INTEGER NOT NULL,
-    FOREIGN KEY (review_id)
-      REFERENCES allReviews (review_id)
+    FOREIGN KEY (char_id)
+      REFERENCES characteristics (char_id)
 );
 
