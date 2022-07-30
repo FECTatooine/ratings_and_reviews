58a4a70c279eab6dd3b30bdd2200cf49fcbb284c
Jean Kim <jeankim119@gmail.com>
2022-07-30 15:09:32 -0500

    can get reviews in mostly correct format, need to reformat date and username

--- db/queries.js
+++ db/queries.js
@@ -1,52 +1,66 @@
 const pool = require('./db.js')
 
 const helpful = (reviewid) => {
+  let querystr = `UPDATE allReviews SET helpful = helpful + 1 WHERE review_id = ${reviewid};`
   return pool
     .connect()
     .then((client) => {
+      console.log('in client')
       return client
-      .query(`UPDATE allReviews
-      SET helpful = helpful + 1
-      WHERE review_id = ${reviewid}`)
+      .query(querystr)
       .then((res) => {client.release(); return res})
       .catch((err) => {client.release(); return err})
       })
 }
 
 const report = (reviewid) => {
+  let querystr = `UPDATE allReviews SET report = true WHERE review_id = ${reviewid};`
   return pool
     .connect()
     .then((client) => {
       return client
-      .query(`UPDATE allReviews
-      SET report = true
-      WHERE review_id = ${reviewid}`)
+      .query(querystr)
       .then((res) => {client.release(); return res})
       .catch((err) => {client.release(); return err})
       })
 }
 
+const getAllReviews = (productid, page, count, sort) => {
+  let offset;
+  if(page === 1){
+   offset = 0;
+  } else {
+    offset = (page - 1) * count;
+  }
+  let querystr = `SELECT allReviews.review_id, allReviews.rating, allReviews.summary, allReviews.recommend, allReviews.response, allReviews.body, allReviews.dateCreated, allReviews.username, allReviews.helpful,
+  CASE WHEN count(photo) = 0 THEN ARRAY[]::json[] ELSE (array_agg(reviewphotos.photo)) END AS photos
+  FROM allReviews
+  LEFT OUTER JOIN
+  (
+    SELECT review_id, json_build_object('id', photo_id, 'url', photo_url) as photo
+    FROM photos
+    ORDER BY photo_id
+  ) reviewphotos
+  ON allReviews.review_id = reviewphotos.review_id
+  WHERE allReviews.product_id = ${productid} AND report = false
+  GROUP BY allReviews.review_id
+  LIMIT ${count}
+  OFFSET ${offset};`
+
+  return pool
+  .connect()
+  .then((client) => {
+    return client
+    .query(querystr)
+    .then((res) => {client.release(); return res})
+    .catch((err) => {client.release(); return err})
+    })
+
+}
+
 exports.helpful = helpful;
 exports.report = report;
-
-// module.exports =  function () {
-//   return {
-//     helpful: function (reviewid) {
-//       console.log('calling helpful')
-//       return 'helpful'
-//         // db.connect()
-//         //   .then(() => {
-//         //     db.query(`UPDATE allReviews
-//         //     SET helpful = helpful + 1
-//         //     WHERE review_id = ${reviewid}`)
-//         //   })
-//         //   .then((res) => {console.log('success'); db.release()})
-//         //   .catch((err) => {console.log('error'); db.release()})
-//     }
-//   }
-// }
-
-
+exports.getAllReviews = getAllReviews;
 
 
 
--- server/app.js
+++ server/app.js
@@ -1,17 +1,37 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const {helpful, report} = require('../db/queries.js');
+const {helpful, report, getAllReviews} = require('../db/queries.js');
+const bodyParser = require('body-parser');
 
 app.listen(port, () => {
   console.log(`server listening on ${port}`)
 })
 app.use(express.json());
 app.use(express.static('client/dist'))
+app.use(bodyParser.json())
 
 
 app.get('/*', function(req, res) {
-  res.send('connected')
+  console.log(req.query)
+  let sort = req.query.sort || 'relevant'
+  let count = req.query.count || 5
+  let page = req.query.page || 1
+  let productid = req.query.product_id
+
+  getAllReviews(productid, page, count, sort)
+    .then((reviews) => {
+      var data = {
+        'product' : productid,
+        'page': page,
+        'count': count,
+        'results': reviews.rows
+      }
+      console.log(data)
+      res.send(data)
+    })
+    .catch((err) => {res.send('500')})
+
 })
 
 app.put('/*', function(req, res) {
